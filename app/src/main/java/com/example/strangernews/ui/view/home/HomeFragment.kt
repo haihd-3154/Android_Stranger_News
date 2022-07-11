c6e3f2f4570178779b2da32affa8b64514e82423
HoDucHaiB <ho.duc.hai-b@sun-asterisk.com>
2022-07-11 08:52:55 +0700

    launch screen, theme, drawer

---
+++ app/src/main/java/com/example/strangernews/ui/view/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.strangernews.ui.view.home
+
+import com.example.strangernews.base.BaseFragment
+import com.example.strangernews.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+}
